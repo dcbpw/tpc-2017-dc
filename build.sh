83d553ee52651eba2c270ceeb3abde36ded56550
Brock Wilcox <bwilcox@optoro.com>
2016-12-13 22:19:44 -0500

    Update build script a bit and minor styling

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 
-ttree -v -r -s src -d docs -f /dev/null --wrapper=./src/head.html --relative
+ttree -v -a -r -s src -d docs -f /dev/null --wrapper=./src/head.html --relative --suffix tt=html
 sass --update src/css/:docs/css
 
 # when-changed src/**/* -c ./build.sh
