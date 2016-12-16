0bb33801cfb9f617f90ade2317c1cec492925fa5
Brock Wilcox <bwilcox@optoro.com>
2016-12-16 08:41:32 -0500

    Move everything to subdir

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 
-ttree -v -a -r -s src -d docs -f /dev/null --wrapper=./src/head.tt --relative --suffix tt=html --copy '.(png|jpg)$'
-sass --update src/css/:docs/css
+ttree -v -a -r -s src/tpc-2017-dc -d docs/tpc-2017-dc -f /dev/null --wrapper=./src/tpc-2017-dc/head.tt --relative --suffix tt=html --copy '.(png|jpg)$'
+sass --update src/tpc-2017-dc/css/:docs/tpc-2017-dc/css
 
