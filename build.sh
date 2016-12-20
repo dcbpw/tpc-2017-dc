fcf4417de62f7d0840576739df07f2eefb1aa109
jeturcotte <joshua.eric.turcotte@gmail.com>
2016-12-20 12:15:32 -0500

    First Draft Aesthetic, needs opinions

--- build.sh
+++
@@ -1,4 +0,0 @@
-
-ttree -v -a -r -s src/tpc-2017-dc -d docs/tpc-2017-dc -f /dev/null --wrapper=./src/tpc-2017-dc/head.tt --relative --suffix tt=html --copy '.(png|jpg)$'
-sass --update src/tpc-2017-dc/css/:docs/tpc-2017-dc/css
-
